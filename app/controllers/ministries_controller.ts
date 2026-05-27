da8736605e880e514517bf451e5cf90c7cae9949
Claude Mutela <claudemutela9@gmail.com>
2026-05-27 11:46:36 +0200

    feat(ministrie) add ministries controller fro display dynamic frontend

---
+++ app/controllers/ministries_controller.ts
@@ -0,0 +1,25 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import Ministry from '#models/ministry'
+import Agenda from '#models/agenda'
+import { DateTime } from 'luxon'
+
+export default class HomeController {
+  public async index({ inertia }: HttpContext) {
+  
+    // Récupère les ministères 
+    const allMinistries = await Ministry.all();
+    // Récupère la dernière prédication (culte dominical)
+   
+
+    return inertia.render('ministries', {
+      allMinistries: allMinistries.map((min) => ({
+        id: min.id,
+        name: min.name,
+        description: min.description,
+        urlImg: min.coverImg || min.urlImg,
+        badgeColor: min.badgeColor,
+        tag: min.tag
+      })),
+    } as never)
+  }
+}

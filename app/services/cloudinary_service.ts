74a97033509ee60c25fad48e6b57f90347be2c3a
Claude Mutela <claudemutela9@gmail.com>
2026-08-08 14:53:53 -0200

    perf(backend): augmentation de la largeur max d'upload à 1920px sans crop forcé

--- app/services/cloudinary_service.ts
+++ app/services/cloudinary_service.ts
@@ -18,7 +18,7 @@ export default class CloudinaryService {
           chunk_size: 6000000, // Tronçonnage par blocs de 6 Mo
           timeout: 120000, // Timeout de 2 minutes
           transformation: [
-            { width: 1200, height: 1200, crop: 'limit' },
+            { width: 1920, crop: 'limit' },
             { quality: 'auto', fetch_format: 'auto' }, // Optimisation automatique et limitation de taille
           ],
         },
